d0e6f5aec1f522c43763984b9cb0ed84f5f444be
BeardedSavage <dangelidakis2015@gamil.com>
2024-07-17 11:03:54 -0500

    New repo to practice node.js and react

---
+++ Outer-Rim_App/src/Components/Applications.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Applications extends Component {
+    constructor () {
+        super ();
+    }
+    render () {
+        return (
+            <>
+            <form>
+                <legend>Eneter Name Here</legend>
+                <input type="text"/>
+            </form>
+            </>
+        );
+    }
+}
+
+export default Applications;
---
+++ Outer-Rim_App/src/Components/Home.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor () {
+        super ();
+    }
+    render() {
+        return (
+            <>
+            <div>
+                <section>
+                    <h1>Welcome to Outer-Rim</h1>
+                    <nav>
+                        <button>Applications</button>
+                    </nav>
+                </section>
+            </div>
+            </>
+        );
+    }
+}
+
+export default Home;
