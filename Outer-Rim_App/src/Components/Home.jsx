3b2ea718c92b4512639a2c81ff550ba75c370f7e
BeardedSavage <dangelidakis2015@gmail.com>
2024-07-23 07:51:04 -0500

    Adding updated files to main branch

--- Outer-Rim_App/src/Components/Home.jsx
+++ Outer-Rim_App/src/Components/Home.jsx
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import Applications from './Applications';
-=======
-import Recommendations from './Recommendations';
->>>>>>> rec-component
 
 class Home extends Component {
     constructor () {
@@ -16,21 +12,10 @@ class Home extends Component {
             <div>
                 <section>
                     <h1>Welcome to Outer-Rim</h1>
-<<<<<<< HEAD
                     <div>
                         <Applications/>
                     </div>
-=======
-                    <nav>
-                        <a href="./Applications">Applications</a>
-                    </nav>
->>>>>>> rec-component
                 </section>
-            </div>
-            <div>
-                <>
-                    <Recommendations/>
-                </>
             </div>
             </>
         );
