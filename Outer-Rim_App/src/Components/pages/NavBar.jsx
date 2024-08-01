ed96fe9d070d31df6a83c85c5937b966eaf94416
BeardedSavage <dangelidakis2015@gmail.com>
2024-08-01 15:45:03 -0500

    fixed image file path

--- Outer-Rim_App/src/Components/pages/NavBar.jsx
+++ Outer-Rim_App/src/Components/pages/NavBar.jsx
@@ -8,7 +8,7 @@ function NavBar () {
         <>
             <div className="nav-container">
                 <Link className="home" to="/"><img 
-                src="https://cdn.discordapp.com/attachments/1155163900216614992/1252017188525572106/Designer-_outer-rim_2.jpg?ex=66a0255f&is=669ed3df&hm=60e830afc6ae8ba40602fe60d809a0ab0dd552443d0954647a6f7f1a1e4a431f&" 
+                src="./images/Designer-_outer-rim_2.jpg"
                 className="logo"/></Link>
                 <ul>
                     <li>
